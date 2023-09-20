fa6ad52959b2c8988e37a6bf3671b80df219cef9
IMWoo94 <leemeawoo94@gmail.com>
2023-09-20 21:47:21 +0900

    spring db part1 jdbc direct connetion use and save 2023.09.20

---
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,9 @@
+package hello.jdbc.connection;
+
+public abstract class ConnectionConst {
+
+	public static final String URL = "jdbc:h2:tcp://localhost/~/study/inflearn/h2DB/springDBpart1/test";
+	public static final String USERNAME = "sa";
+	public static final String PASSWORD = "";
+
+}
