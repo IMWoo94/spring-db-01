618dd0f0481ab7330e25eecf1fe93679a711a2ea
IMWoo94 <leemeawoo94@gmail.com>
2023-09-23 17:12:36 +0900

    spring db part1 jdbctemplate use 2023.09.23

--- src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -18,7 +18,7 @@ import org.springframework.context.annotation.Bean;
 
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepository;
-import hello.jdbc.repository.MemberRepositoryV4_2;
+import hello.jdbc.repository.MemberRepositoryV5;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -59,7 +59,8 @@ class MemberServiceV4Test {
 		@Bean
 		MemberRepository memberRepository() {
 			// return new MemberRepositoryV4_1(dataSource);
-			return new MemberRepositoryV4_2(dataSource);
+			// return new MemberRepositoryV4_2(dataSource);
+			return new MemberRepositoryV5(dataSource);
 		}
 
 		@Bean
